fd93ab03a29ca18bfe07c1e0e0eff3dadd701f24
Kevin Hsieh <bqh5026@Kevins-iMac.local>
2019-08-12 16:03:11 -0400

    Updates

---
+++ precache-manifest.c2d7f05389cbe2408e6f17740251b692.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619ea1ce4c8a4eb944e1cda1e73c7328",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "084b7f6ef6f8f1b3c9a4",
+    "url": "/monsters-rolodex/static/css/main.1beed8d2.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "084b7f6ef6f8f1b3c9a4",
+    "url": "/monsters-rolodex/static/js/main.06820587.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
